0f5e17f4db86cf61ee5083d1dba71ba953891a86
Ju Lacerda <julacerdarod@gmail.com>
2022-04-28 15:19:00 +1000

    Showing the current date

--- src/script.js
+++ src/script.js
@@ -1,26 +1,6 @@
-function displayTemperature(response) {
-  console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-}
-
-let apiKey = "5f1fe36cdfe3cb520d72b14cd7881d3e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brisbane&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
-
-axios.get(apiUrl).then(displayTemperature);
-
-function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -44,6 +24,30 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "5f1fe36cdfe3cb520d72b14cd7881d3e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brisbane&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
 
 function search(event) {
   event.preventDefault();
